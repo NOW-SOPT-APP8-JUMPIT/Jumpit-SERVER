2c19c68ad3556324446b3eabf3a261ac82bd3d57
geniuus <cross_man@naver.com>
2024-05-15 21:22:47 +0900

    Feat [#5] Company-findById 구현

--- jumpit/src/main/java/org/sopt/jumpit/company/repository/CompanyRepository.java
+++ jumpit/src/main/java/org/sopt/jumpit/company/repository/CompanyRepository.java
@@ -1,4 +1,4 @@
-package org.sopt.jumpit.company.repository.repository;
+package org.sopt.jumpit.company.repository;
 
 import org.sopt.jumpit.company.domain.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
