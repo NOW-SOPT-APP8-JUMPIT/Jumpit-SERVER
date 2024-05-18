4c53d0ec7c7ccfc2631fe3ce48bd8f9e8641f53f
정채은 <cce523@hanmail.net>
2024-05-19 05:41:03 +0900

    Feat [#13] 컨트롤러 구현

--- jumpit/src/main/java/org/sopt/jumpit/resume/controller/ResumeController.java
+++ jumpit/src/main/java/org/sopt/jumpit/resume/controller/ResumeController.java
@@ -3,16 +3,11 @@ package org.sopt.jumpit.resume.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.sopt.jumpit.global.common.dto.SuccessResponse;
+import org.sopt.jumpit.resume.domain.Resume;
 import org.sopt.jumpit.resume.dto.ResumeCreateRequest;
 import org.sopt.jumpit.resume.service.ResumeService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.net.URI;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/resumes")
@@ -27,5 +22,10 @@ public class ResumeController {
         resumeService.createResume(resumeCreateRequest);
         return ResponseEntity.ok()
                 .build();
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<Resume> findResumeById(@PathVariable Long userId) {
+        return ResponseEntity.ok(resumeService.findResumeById(userId));
     }
 }
