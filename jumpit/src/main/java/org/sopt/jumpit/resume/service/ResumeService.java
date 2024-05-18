2953edb705a00028d97caf49d34eda14dfbf9776
정채은 <cce523@hanmail.net>
2024-05-19 04:08:47 +0900

    Feat [#10] 서비스 구현

---
+++ jumpit/src/main/java/org/sopt/jumpit/resume/service/ResumeService.java
@@ -0,0 +1,30 @@
+package org.sopt.jumpit.resume.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.sopt.jumpit.global.common.dto.message.ErrorMessage;
+import org.sopt.jumpit.global.exception.BusinessException;
+import org.sopt.jumpit.resume.domain.Resume;
+import org.sopt.jumpit.resume.dto.ResumeCreateRequest;
+import org.sopt.jumpit.resume.repository.ResumeRepository;
+import org.sopt.jumpit.user.domain.User;
+import org.sopt.jumpit.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ResumeService {
+    private final ResumeRepository resumeRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void createResume(
+            ResumeCreateRequest resumeCreateRequest
+    ) {
+        User findUser = userRepository.findById(resumeCreateRequest.userId()).orElseThrow(
+                () -> new BusinessException(ErrorMessage.SEARCH_FAILED)
+        );
+        Resume resume = Resume.create(findUser, resumeCreateRequest.title());
+        resumeRepository.save(resume);
+    }
+}
