53b5f528235a68014246c2d00df4e544a0092c7b
정채은 <cce523@hanmail.net>
2024-05-21 05:18:50 +0900

    Feat [#14] 이력서 리스트 형태로 반환하도록 수정

--- jumpit/src/main/java/org/sopt/jumpit/resume/service/ResumeService.java
+++ jumpit/src/main/java/org/sopt/jumpit/resume/service/ResumeService.java
@@ -7,10 +7,15 @@ import org.sopt.jumpit.global.exception.NotFoundException;
 import org.sopt.jumpit.resume.domain.Resume;
 import org.sopt.jumpit.resume.dto.ResumeCreateRequest;
 import org.sopt.jumpit.resume.dto.ResumePrivateRequest;
+import org.sopt.jumpit.resume.dto.ResumeResponse;
+import org.sopt.jumpit.resume.dto.ResumeSearchResponse;
 import org.sopt.jumpit.resume.repository.ResumeRepository;
 import org.sopt.jumpit.user.domain.User;
 import org.sopt.jumpit.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -30,10 +35,21 @@ public class ResumeService {
         return resume.getId();
     }
 
-    public Resume findResumeById(Long resumeId) {
-        return resumeRepository.findById(resumeId).orElseThrow(
+    public ResumeSearchResponse findResumeById(Long userId) {
+        List<ResumeResponse> resumes = resumeRepository.findAll().stream()
+                .map(
+                        resume -> new ResumeResponse(
+                                resume.getId(),
+                                resume.getTitle(),
+                                resume.isPrivate(),
+                                resume.getCreatedAt(),
+                                resume.getModifiedAt()
+                        )
+                ).collect(Collectors.toList());
+        userRepository.findById(userId).orElseThrow(
                 () -> new NotFoundException(ErrorMessage.USER_NOT_FOUND_BY_ID_EXCEPTION)
         );
+        return new ResumeSearchResponse(userId, resumes);
     }
 
     @Transactional
