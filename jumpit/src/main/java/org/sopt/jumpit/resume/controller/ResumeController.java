07059a327749f0da82498b2df6392f41bb2eaa54
정채은 <cce523@hanmail.net>
2024-05-21 05:21:07 +0900

    Fix [#14] 전체 반환 값 수정

--- jumpit/src/main/java/org/sopt/jumpit/resume/controller/ResumeController.java
+++ jumpit/src/main/java/org/sopt/jumpit/resume/controller/ResumeController.java
@@ -3,13 +3,17 @@ package org.sopt.jumpit.resume.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.sopt.jumpit.global.common.dto.SuccessResponse;
+import org.sopt.jumpit.global.common.dto.message.SuccessMessage;
 import org.sopt.jumpit.resume.domain.Resume;
 import org.sopt.jumpit.resume.dto.ResumeCreateRequest;
 import org.sopt.jumpit.resume.dto.ResumePrivateRequest;
 import org.sopt.jumpit.resume.dto.ResumeSearchResponse;
 import org.sopt.jumpit.resume.service.ResumeService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -21,14 +25,18 @@ public class ResumeController {
     public ResponseEntity<SuccessResponse> createResume (
             @RequestBody ResumeCreateRequest resumeCreateRequest
     ) {
-        resumeService.createResume(resumeCreateRequest);
-        return ResponseEntity.ok()
-                .build();
+        Long resumeId = resumeService.createResume(resumeCreateRequest);
+        URI location = URI.create("/api/v1/resumes/" + resumeId);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .location(location)
+                .body(SuccessResponse.of(SuccessMessage.RESUME_CREATED_COMPLETED_SUCCESS));
     }
 
     @GetMapping("/resumes/{userId}")
-    public ResponseEntity<Resume> findResumeById(@PathVariable Long userId) {
-        return ResponseEntity.ok(resumeService.findResumeById(userId));
+    public ResponseEntity<SuccessResponse<ResumeSearchResponse>> findResumeById(@PathVariable Long userId) {
+        ResumeSearchResponse resumeSearchResponse = resumeService.findResumeById(userId);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(SuccessResponse.of(SuccessMessage.RESUME_SEARCH_COMPLETED_SUCCESS, resumeSearchResponse));
     }
 
     @PatchMapping("/resumes/{resumeId}")
@@ -37,7 +45,7 @@ public class ResumeController {
             @RequestBody ResumePrivateRequest request
     ) {
         resumeService.updateResumePrivate(resumeId, request);
-        return ResponseEntity.noContent()
-                .build();
+        return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                .body(SuccessResponse.of(SuccessMessage.RESUME_PRIVATE_CHANGE_COMPLETED_SUCCESS));
     }
 }
