7e547b56ca85429ba5d7a8487fca919f93972dc8
geniuus <cross_man@naver.com>
2024-05-21 22:59:33 +0900

    Fix [#14] Default Constructor 이슈 해결
    @NoArgsConstructor 어노테이션 추가

--- jumpit/src/main/java/org/sopt/jumpit/resume/domain/Resume.java
+++ jumpit/src/main/java/org/sopt/jumpit/resume/domain/Resume.java
@@ -3,6 +3,7 @@ package org.sopt.jumpit.resume.domain;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.sopt.jumpit.resume.dto.ResumeCreateRequest;
 import org.sopt.jumpit.user.domain.User;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @Table(name = "Resume")
 @Getter
 @Setter
+@NoArgsConstructor
 public class Resume {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
